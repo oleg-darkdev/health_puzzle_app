5d6ecdb9b1e461accaa0f2279677a02320ffc78f
DarkDev <darkdev_brand@riseup.net>
2024-10-11 21:35:52 +0300

    stable v.

---
+++ src/lib/shared/ui/OtherboardgameContent.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let boardgame;
+</script>
+
+<div>
+  <div class="">
+    <img src="{boardgame.logo}" class='h-16 w-auto' alt="">
+  </div>
+  <div class="work_text">
+    <div class="textweight_medium">
+      <div class="text_size_m">{boardgame.title}</div>
+    </div>
+    {#each boardgame.desc as desc}
+      <p class="text_size_s">
+        {desc}
+      </p>
+    {/each}
+  </div>
+</div>
+
+  <style>
+
+  </style>
