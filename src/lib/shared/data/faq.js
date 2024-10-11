aa66a360d8d90f86b7c932a2f1001bfd0d06b323
DarkDev <darkdev_brand@riseup.net>
2024-10-11 21:35:39 +0300

    upld data for components

---
+++ src/lib/shared/data/faq.js
@@ -0,0 +1,87 @@
+const faq = [
+  {
+    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    a: [
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    ],
+    cssClass: 'small-faq-card',
+    id: 1
+  },
+  {
+    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    a: [
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    ],
+    cssClass: 'small-faq-card',
+    id: 2
+  },
+  {
+    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    a: [
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    ],
+    cssClass: 'small-faq-card',
+    id: 3
+  },
+  {
+    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    a: [
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    ],
+    cssClass: 'small-faq-card',
+    id: 4
+  },
+  {
+    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    a: [
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    ],
+    cssClass: 'small-faq-card',
+    id: 5
+  },
+  {
+    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    a: [
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    ],
+    cssClass: 'small-faq-card',
+    id: 6
+  },
+  {
+    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    a: [
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    ],
+    cssClass: 'big-faq-card',
+    id: 7
+  },
+  {
+    q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    a: [
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    ],
+    cssClass: 'big-faq-card',
+    id: 8
+  },
+
+
+]
+
+
+export default faq;
---
+++ src/lib/shared/data/navigation.js
@@ -0,0 +1,29 @@
+let links = [
+    {
+      title: 'O grze',
+      link: '#about'
+    },
+    {
+      title: 'Dla kogo',
+      link: '#target-audience'
+    },
+    {
+      title: 'Scenariusze',
+      link: '#scenarios'
+    },
+    {
+      title: 'O autorze',
+      link: '#author'
+    },
+    {
+      title: 'F.A.Q.',
+      link: '#faq'
+    },
+    // {
+    //   title: 'Koszty',
+    //   link: '#pricing'
+    // },
+  ]
+
+
+export default links;
