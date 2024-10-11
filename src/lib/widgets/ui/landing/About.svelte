41aa342cdcf34a66c07448e7a00cec67ed71deb4
DarkDev <darkdev_brand@riseup.net>
2024-10-11 21:36:02 +0300

    stable v.

---
+++ src/lib/widgets/ui/landing/About.svelte
@@ -0,0 +1,40 @@
+<script>
+
+</script>
+
+<section class="section">
+  <div class="about_bgfade"></div>
+  <div class="w-layout-blockcontainer container _60 w-container">
+    <div class="comp_about">
+      <div id="about" class="anchor"></div>
+      <div class="section-title">
+        <div class="text_heading_m">O co chodzi w tym narzędziu</div>
+      </div>
+      <div  style="" class="flex-v _1">
+        <div class="text_size_m">
+          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </div>
+        <div class="text_size_m">
+          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </div>
+      </div>
+      <div style="" class="about_bg w-embed">
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 763 362" fill="none" preserveAspectRatio="xMidYMid meet" aria-hidden="true" role="img">
+          <ellipse cx="381.533" cy="181" rx="381" ry="181" fill="url(#paint0_linear_4007_67020)"></ellipse>
+          <defs>
+            <linearGradient id="paint0_linear_4007_67020" x1="141.5" y1="-55.5" x2="409.202" y2="548.431" gradientUnits="userSpaceOnUse">
+              <stop offset="0.02" stop-color="#30F9D5"></stop>
+              <stop offset="0.51046" stop-color="#94E5FF"></stop>
+              <stop offset="0.96" stop-color="#94CCFF"></stop>
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>
