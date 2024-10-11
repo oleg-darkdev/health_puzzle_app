de9cde8d1583b5c412f4e62f53dc2fca1d16ac3c
DarkDev <darkdev_brand@riseup.net>
2024-10-11 21:36:06 +0300

    stable v.

---
+++ src/lib/widgets/ui/landing/AboutAuthor.svelte
@@ -0,0 +1,56 @@
+<script>
+
+</script>
+
+<section class="section">
+    <div class="w-layout-blockcontainer container w-container">
+        <div class="comp_course">
+            <div class="section-title">
+                <div class="text_heading_m"><span>Lorem impsum - <a href='https://oleg-darkdev.vercel.app/' class='text-style-link' target="_blank" > Oleg_darkdev </a></span></div>
+            </div>
+            <div class="layout_grid2">
+                <div   style="" class="course_text w-richtext">
+                    <ul role="list">
+                        <li>
+                            <strong>Product Web Engineer-IT Enthusiast, Gamification Coach, and JavaScript Programmer with across various technologies, <span class="text-weight-bold"> years of experience in board game development and 3 years in web application development</span> including startups and open-source projects. See my open sorce projects are available on
+                                <span class="text-weight-bold"><a class='text-style-link ' href='https://github.com/oleg-darkdev' target="_blank">github profile.</a></span></strong>
+                        </li>
+                        <li class=''>
+                            <strong class=''>
+                              Engaged in merging the digital and physical worlds to create engaging and educational experiences. Сollaborated with several <span class="text-weight-bold"> NGOs and museums in Belarus and Poland </span> on various historical projects.
+                            </strong>
+                        </li>
+                        <li>
+                            <strong>
+                                See my other historical projects and <span class="text-weight-bold"> educational board games that combine history and modern technologies in my <a class='text-style-link ' href='https://oleg-darkdev.vercel.app/' target="_blank"> portfolio.</a>
+                                </span>
+                            </strong>
+                        </li>
+                        <li>
+                            <strong>
+                              If you have ideas for collaboration - <span class="text-weight-bold">say me Hello from <a class='text-style-link ' href='https://www.linkedin.com/in/oleg-darkdev' target="_blank">linkedin.</a></span>
+                              <br />
+                              In head with a love for technologies, in heart with a love for people 🖤
+                            </strong>
+                        </li>
+                        <a href='https://oleg-darkdev.vercel.app/' target="_blank" class="button w-button">Get in touch</a>
+<!--
+                        <li>
+                            <strong>Глибинний інтерес та особистий досвід практики медитації в світському та глибинному підходах: </strong>
+                            <ul role="list">
+                                <li>7 років та понад 5 тис. годин індивідуальної практики.</li>
+                            </ul>
+                        </li> -->
+
+                    </ul>
+                </div>
+                <img src="/images/author_1.png" loading="lazy" width="Auto" height="Auto" alt="" class="image_100h">
+            </div>
+            <div id="author" class="anchor"></div>
+        </div>
+    </div>
+</section>
+
+<style>
+
+</style>
