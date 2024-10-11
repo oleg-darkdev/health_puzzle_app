dd8a4850c9fe4dfc8ecf8e47572b2b4c1f097635
DarkDev <darkdev_brand@riseup.net>
2024-10-11 21:36:44 +0300

    proto v.

---
+++ src/lib/widgets/ui/landing/FreeSecond.svelte
@@ -0,0 +1,85 @@
+<script>
+
+
+</script>
+
+
+<section class="section">
+  <div class="w-layout-blockcontainer container w-container">
+    <div class="comp_quotes2">
+      <div  style="" class="chip programme">
+        <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </div>
+      </div>
+      <div data-delay="4000" data-animation="cross" class="quotes_slider alt w-slider" data-autoplay="false" data-easing="ease" style="" data-hide-arrows="false" data-disable-swipe="false" data-w-id="48ac155f-4dfe-828a-77df-bd19f66516ac" data-autoplay-limit="0" data-nav-spacing="3" data-duration="500" data-infinite="true">
+        <div class="w-slider-mask">
+          <!-- <div class="w-slide">
+            <div class="quotes_single">
+              <div class="quotes_content">
+                <div class="avatar">
+                  <img src="images/667aaf166b490f9c929c0525_Quote.jpg" loading="lazy" width="64" height="64" alt="" class="avatar_img alt">
+                  <div class="avatar_content">
+                    <div class="textweight_medium">
+                      <div class="text_size_m">Олена Лук'янчук</div>
+                    </div>
+                    <div class="opacity _60">
+                      <div class="text_size_xs">Head of Operational Services Division at Raiffeisen Bank Ukraine · Київ, Україна</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="quotes-wrap">
+                  <div class="quote">Я долучилася до курсу через рекомендацію. Навіть не знаючи, у чому полягають інструменти та практики курсу, мене привабив опис курсу, який містив роз'яснення про роль уважності у всіх сферах нашого життя. Я натрапила там на опис різних навичок напряму уваги: фокус-розфокус, концентрація-деконцентрація, роль навички уважності до своїх почуттів, розуму, емоцій та відчуттів у тілі. <br>
+                    <br>Опис курсу збігся з моїм особистим запитом на той час, а сам курс перевищив мої очікування. Було дуже цікаво та корисно дізнаватися про те, як саморозвиток пов'язаний з увагою, отримати матеріали для розширення знань та світогляду у цій сфері. Окрема подяка за практичні заняття. Щиро вважаю, що курс вплинув не лише на мої цілі, а й на щоденне самовідчуття, можливість почуватися в моменті, відновлювати та регулювати свій стан. Мої колеги та друзі помітили ті зміни в мені, що для мене важливі. <br>
+                    <br>Вважаю, що мені важливо й надалі розвивати навички свідомого спрямовування уваги, тому зареєструвалася на довгий курс Mindful Leadership. <br>
+                  </div>
+                  <div class="quotes_icon r cyan w-embed">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 25 19" fill="none" preserveAspectRatio="xMidYMid meet" aria-hidden="true" role="img">
+                      <path d="M15.7277 4.77809C16.2077 2.07809 18.6077 0.0380859 21.3077 0.0380859C24.0077 0.0380859 25.5077 3.15809 24.7877 7.17809C23.2277 16.0581 18.0677 18.8181 12.6077 18.8181L12.8477 13.8981C17.8277 13.8981 21.3077 11.9781 22.6877 8.25809C21.9677 8.97809 20.7677 9.45809 19.6277 9.45809C16.9277 9.45809 15.2477 7.47809 15.7277 4.77809ZM5.52767 4.77809C6.00767 2.07809 8.40767 0.0380859 11.1077 0.0380859C13.8077 0.0380859 15.3077 3.15809 14.5877 7.17809C13.0277 16.0581 7.02767 18.8181 0.607666 18.8181L0.847666 13.8981C6.84767 13.8981 10.9877 11.9781 12.4877 8.25809C11.7677 8.97809 10.5677 9.45809 9.42767 9.45809C6.72767 9.45809 5.04767 7.47809 5.52767 4.77809Z" fill="currentColor"></path>
+                    </svg>
+                  </div>
+                  <div class="quotes_icon cyan w-embed">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 25 19" fill="none" preserveAspectRatio="xMidYMid meet" aria-hidden="true" role="img">
+                      <path d="M9.47363 14.0781C8.99363 16.7781 6.59363 18.8181 3.89363 18.8181C1.19363 18.8181 -0.306366 15.6981 0.413634 11.6781C1.97363 2.79809 7.13363 0.0380859 12.5936 0.0380859L12.3536 4.95809C7.37364 4.95809 3.89363 6.87809 2.51363 10.5981C3.23363 9.87809 4.43363 9.39809 5.57364 9.39809C8.27363 9.39809 9.95363 11.3781 9.47363 14.0781ZM19.6736 14.0781C19.1936 16.7781 16.7936 18.8181 14.0936 18.8181C11.3936 18.8181 9.89363 15.6981 10.6136 11.6781C12.1736 2.79809 18.1736 0.0380859 24.5936 0.0380859L24.3536 4.95809C18.3536 4.95809 14.2136 6.87809 12.7136 10.5981C13.4336 9.87809 14.6336 9.39809 15.7736 9.39809C18.4736 9.39809 20.1536 11.3781 19.6736 14.0781Z" fill="currentColor"></path>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> -->
+
+        </div>
+        <div class="slider_arrow cyan w-slider-arrow-left">
+          <div class="icon-embed-1 w-embed">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 16 16" fill="none" preserveAspectRatio="xMidYMid meet" aria-hidden="true" role="img">
+              <path d="M10.4999 2.99991V12.9999C10.5 13.0989 10.4707 13.1956 10.4158 13.2779C10.3608 13.3602 10.2827 13.4243 10.1913 13.4622C10.0999 13.5001 9.99929 13.51 9.90225 13.4907C9.80521 13.4713 9.71608 13.4237 9.64615 13.3537L4.64615 8.35366C4.59967 8.30722 4.56279 8.25207 4.53763 8.19138C4.51246 8.13068 4.49951 8.06561 4.49951 7.99991C4.49951 7.9342 4.51246 7.86913 4.53763 7.80844C4.56279 7.74774 4.59967 7.69259 4.64615 7.64616L9.64615 2.64616C9.71608 2.57615 9.80521 2.52847 9.90225 2.50914C9.99929 2.48982 10.0999 2.49972 10.1913 2.53759C10.2827 2.57547 10.3608 2.63962 10.4158 2.72191C10.4707 2.80421 10.5 2.90096 10.4999 2.99991Z" fill="currentColor"></path>
+            </svg>
+          </div>
+        </div>
+        <div class="slider_arrow r cyan w-slider-arrow-right">
+          <div class="icon-embed-1 w-embed">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 16 16" fill="none" preserveAspectRatio="xMidYMid meet" aria-hidden="true" role="img">
+              <path d="M11.3538 8.35366L6.35375 13.3537C6.28382 13.4237 6.1947 13.4713 6.09765 13.4907C6.00061 13.51 5.90002 13.5001 5.8086 13.4622C5.71719 13.4243 5.63908 13.3602 5.58414 13.2779C5.5292 13.1956 5.49992 13.0989 5.5 12.9999V2.99991C5.49992 2.90096 5.5292 2.80421 5.58414 2.72191C5.63908 2.63962 5.71719 2.57547 5.8086 2.53759C5.90002 2.49972 6.00061 2.48982 6.09765 2.50914C6.1947 2.52847 6.28382 2.57615 6.35375 2.64616L11.3538 7.64616C11.4002 7.69259 11.4371 7.74774 11.4623 7.80844C11.4874 7.86913 11.5004 7.9342 11.5004 7.99991C11.5004 8.06561 11.4874 8.13068 11.4623 8.19138C11.4371 8.25207 11.4002 8.30722 11.3538 8.35366Z" fill="currentColor"></path>
+            </svg>
+          </div>
+        </div>
+        <div class="hide w-slider-nav w-round w-num"></div>
+      </div>
+      <div id="quotes-2" class="anchor"></div>
+    </div>
+    <div class="quotes_bg w-embed">
+      <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 983 678" fill="none" preserveAspectRatio="xMidYMid meet" aria-hidden="true" role="img">
+        <ellipse cx="491.5" cy="339" rx="491.5" ry="339" fill="url(#paint0_linear_4024_1152)"></ellipse>
+        <defs>
+          <linearGradient id="paint0_linear_4024_1152" x1="476.57" y1="10.7096" x2="629.124" y2="628.701" gradientUnits="userSpaceOnUse">
+            <stop offset="0.02" stop-color="#30F9D5"></stop>
+            <stop offset="1" stop-color="#94DFFF"></stop>
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>
