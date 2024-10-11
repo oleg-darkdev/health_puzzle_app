df78460be1489b4ea2214d27ecb5fd150b47819b
DarkDev <darkdev_brand@riseup.net>
2024-10-11 21:37:00 +0300

    alfa v.

---
+++ src/lib/widgets/ui/landing/Stats.svelte
@@ -0,0 +1,52 @@
+<script>
+
+</script>
+
+<div style="" class="hero_stat">
+    <div class="hero_stat_single">
+        <div class="hero_stat_label">
+            <div class="text_size_s">Lorem Ipsum  </div>
+        </div>
+        <div class="hero_stat_num">
+            <div class="text_heading_s">120</div>
+            <div class="text_heading_s">
+                <span class="textstyle_outlined">Lorem </span>
+            </div>
+        </div>
+    </div>
+    <div class="hero_stat_single">
+        <div class="hero_stat_label">
+            <div class="text_size_s">Lorem Ipsum is</div>
+        </div>
+        <div class="hero_stat_num">
+            <div class="text_heading_s">80%</div>
+            <div class="text_heading_s">
+                <span class="textstyle_outlined">Lorem</span>
+            </div>
+        </div>
+    </div>
+    <div class="hero_stat_single">
+        <div class="hero_stat_label">
+            <div class="text_size_s">Lorem Ipsum</div>
+        </div>
+        <div class="hero_stat_num">
+            <div class="text_heading_s">Lorem</div>
+        </div>
+    </div>
+    <div class="hero_stat_single">
+        <div class="hero_stat_label">
+            <div class="text_size_s">Lorem Ipsum</div>
+        </div>
+        <div class="hero_stat_num">
+            <div class="text_heading_s">30</div>
+            <div class="text_heading_s">
+                <span class="textstyle_outlined">Lorem</span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<style>
+
+</style>
