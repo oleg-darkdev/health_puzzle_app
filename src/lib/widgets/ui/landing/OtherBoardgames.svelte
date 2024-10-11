b92d946e7a8f44e253032ccf3f72ca22d9e858f9
DarkDev <darkdev_brand@riseup.net>
2024-10-11 21:37:47 +0300

    stable v.

---
+++ src/lib/widgets/ui/landing/OtherBoardgames.svelte
@@ -0,0 +1,26 @@
+<script>
+ 
+</script>
+
+<section class="section">
+  <div class="w-layout-blockcontainer container w-container">
+    <div class="comp_work">
+      <div class="section-title">
+        <!-- Inne gry planszowe -->
+        <div class="text_heading_m">Puzzle management.</div>
+        <div class="-mb-10">
+          <div class="text_size_m">lista innych gier edukacyjnych objętych projektem: <a href="/" style='font-weight: 400;' class='textstyle_spaciousblackit' target="_blank"> puzzle management </a>.</div>
+        </div>
+      </div>
+      <div  class="work_grid">
+        <slot />
+      </div>
+      <div id="other-boardgames" class="anchor"></div>
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>
